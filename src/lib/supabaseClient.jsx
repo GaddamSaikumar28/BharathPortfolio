144e33e997b986670f1c2295d999db80bb72b523
saikumar <gaddamsaikumar12345@gmail.com>
2025-11-09 21:53:15 +0530

    client

--- src/lib/supabaseClient.jsx
+++ src/lib/supabaseClient.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // IMPORTANT: Add your project URL and anon key here
-const supabaseUrl = 'https://qxjzhtugsgpyvwhcomhi.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4anpodHVnc2dweXZ3aGNvbWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1Nzg4NTksImV4cCI6MjA3ODE1NDg1OX0.vlKslwBD-2EYBAVVXrcQ4ViY5VwgPfQTrDnJAOoPE-w'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
